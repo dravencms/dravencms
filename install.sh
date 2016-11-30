dd07e8c3c108219427b6c8f5fe6cfcefb73c9a48
Adam Schubert <adam.schubert@sg1-game.net>
2016-11-30 04:09:28 +0100

    Set project name as first parameters

--- install.sh
+++ install.sh
@@ -1,1 +1,2 @@
-composer create-project dravencms/dravencms:@dev test-dravencms --repository '{"type":"vcs","url":"git@gitlab.salamek.cz:sadam/dravencms-dravencms.git"}'
+#!/bin/bash
+composer create-project dravencms/dravencms:@dev $1 --repository '{"type":"vcs","url":"git@gitlab.salamek.cz:sadam/dravencms-dravencms.git"}'
