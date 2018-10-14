07076c0c63c2493f63fb990a48739638798a3402
Adam Schubert <adam.schubert@sg1-game.net>
2018-10-15 00:27:15 +0200

    Compability with PHP buildin webserver

--- www/index.php
+++ www/index.php
@@ -1,5 +1,13 @@
 <?php
 
+if (php_sapi_name() == 'cli-server') {
+    $_SERVER['SCRIPT_NAME'] = '/index.php';
+    $parsedUrl = parse_url($_SERVER["REQUEST_URI"]);
+    if (is_file(__DIR__.$parsedUrl['path'])) {
+        return false;    // serve the requested resource as-is.
+    }
+}
+
 // Uncomment this line if you must temporarily take down your site for maintenance.
 // require '.maintenance.php';
 
